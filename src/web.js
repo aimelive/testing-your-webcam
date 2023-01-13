8cca3be52669ec3177ed59ae28d1b4073b68fd5c
aimelive <aimendayambaje24@gmail.com>
2023-01-13 14:59:23 +0200

    Time counting

--- src/web.js
+++ src/web.js
@@ -1,47 +1,83 @@
+const video = document.getElementById("video");
+const vendorURL = window.URL || window.webkitURL;
+let video_outer = document.querySelector(".video-outer");
+let durationSpan = document.querySelector(".duration");
+const div = document.querySelector(".recording-status");
 
-   const video = document.getElementById("video");
-   const vendorURL = window.URL || window.webkitURL;
-   let video_outer = document.querySelector(".video-outer")
-   let downloadBtn = document.createElement('a')
-   let isCameraOn = false;
+let isCameraOn = false;
 
-  const stop =  ()=> {
-    if(!isCameraOn) return;
-    var stream = video.srcObject;
-    var tracks = stream.getTracks();
-   
-    for (var i = 0; i < tracks.length; i++) {
-        var track = tracks[i];
-        track.stop();
-    }
-   isCameraOn = false;
-    video.srcObject = null;
-    video_outer.innerHTML = "<p class='done'> Camera stopped</p><button onclick='location.reload();' class='btn-reload'>Record again</button>";
-    notifyUser()
-}
 
-const start =  ()=> {
-    if(isCameraOn) return;
-    if (navigator.mediaDevices.getUserMedia) {
-        navigator.mediaDevices.getUserMedia({ video: true })
-            .then(function (stream) {
-                video.srcObject = stream;
-                isCameraOn = true;
-            }).catch(function (error) {
-              console.log(error)
-                video_outer.innerHTML = "<p class='wrong'> Something went wrong! Camera access denied</p>";
-            });
-    }
-}
+const start = () => {
+  if (isCameraOn) return;
+  if (navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices
+      .getUserMedia({ video: true })
+      .then(function (stream) {
+        div.style.display = "flex";
+        video.srcObject = stream;
+        isCameraOn = true;
+        timeCounter();
+      })
+      .catch(function (error) {
+        console.log(error);
+        video_outer.innerHTML =
+          "<p class='wrong'> Something went wrong! Camera access denied</p>";
+      });
+  }
+};
 
+const stopCam = () => {
+  if (!isCameraOn) return;
+  var stream = video.srcObject;
+  var tracks = stream.getTracks();
+
+  for (var i = 0; i < tracks.length; i++) {
+    var track = tracks[i];
+    track.stop();
+  }
+  isCameraOn = false;
+  video.srcObject = null;
+  video_outer.innerHTML =
+    "<p class='done'> Camera stopped</p><button onclick='location.reload();' class='btn-reload'>Record again</button>";
+  div.style.display = "none";
+  notifyUser();
+};
 
 function openFullscreen() {
-    if(!isCameraOn) return; 
-    if (video.requestFullscreen) {
-      video.requestFullscreen();
-    } else if (video.webkitRequestFullscreen) { /* Safari */
-      video.webkitRequestFullscreen();
-    } else if (video.msRequestFullscreen) { /* IE11 */
-      video.msRequestFullscreen();
+  if (!isCameraOn) return;
+  if (video.requestFullscreen) {
+    video.requestFullscreen();
+  } else if (video.webkitRequestFullscreen) {
+    /* Safari */
+    video.webkitRequestFullscreen();
+  } else if (video.msRequestFullscreen) {
+    /* IE11 */
+    video.msRequestFullscreen();
+  }
+}
+
+function timeCounter() {
+  let interval;
+  if (isCameraOn) {
+    let sec = 0;
+    let min = 0;
+    interval = setInterval(() => {
+      durationSpan.innerHTML = `${min < 10 ? "0" + min : min}:${
+        sec < 10 ? "0" + sec : sec
+      }`;
+      sec++;
+      if (sec > 59) {
+        min++;
+        sec = 0;
+      }
+      if (min === 10) {
+        stopCam();
+      }
+    }, 1000);
+  } else {
+    if (interval) {
+      durationSpan.innerHTML = "00:00";
+      clearInterval(interval);
     }
   }
+}
